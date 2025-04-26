54f4b6f872db79d77cb9a65c5c2ce0064c0a0a49
Maereg <maeregzewdu2020@gmail.com>
2025-04-26 21:04:54 +0300

    minor update to the routes file

--- routes/web.php
+++ routes/web.php
@@ -95,10 +95,10 @@ Route::middleware('auth')->prefix('api')->group(function() {
 });
 
 Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
 Route::middleware('auth')->group(function() {
     Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
     Route::post('/projects/create', [ProjectController::class, 'store'])->name('projects.store');
-    Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
     Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
     Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
     Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
